46e14a2f76c98f0c4749efa7478f95d6f0c08af6
TaeHoon Yoon <thyoon220@gmail.com>
2014-12-14 20:35:53 -0800

    Added basic layout to main page + image slider.
    -index.html now contains the basic structure of header/image slide/button
    -index.html contains two test images in the div class fadein. These images were for the purpose of testing the slide
    -Slide show occurs every two seconds with 1 second fade in time and 2 second fade out time (jquery in main.js)
    -basic css for header implemented.

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+$('.fadein img:gt(0)').hide();
+
+$(function(){
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+      $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');}, 
+      3000);
+});
