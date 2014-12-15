955b6e5dd4655c47339b6e348e1ec811e5b1fe83
Kenny Czadzeck <kennycgit@gmail.com>
2014-12-14 19:19:17 -0800

    Successfully added API GET request and function to parse static image URLs

---
+++ public/js/imgurAPI.js
@@ -0,0 +1,25 @@
+var imageLinks = new Arrray;
+var imageObjects = new Array;
+
+var getImages = function(){
+  $.ajax({
+      type: "GET",
+      url: "https://api.imgur.com/3/gallery/r/aww.json",
+      dataType: "json",
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader('Authorization', 'Client-ID '+ apiKey +'');
+      },
+      success: function(data) {
+        imageObjects = data.data
+      }
+    });
+};
+
+var filterLinks = function(){
+  for (var i = 0; i < imageObjects.length; i++) {
+    var image = imageObjects[i].link
+    if (imageObjects[i].animated === false) {
+      imageLinks.push(image)
+    }
+  };
+};
