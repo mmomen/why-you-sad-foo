73732aa92f1a1b62dd98327ac29220f0966334e2
TaeHoon Yoon <thyoon220@gmail.com>
2014-12-15 16:57:59 -0800

    Added sticky nav bar functionality in main page.
    -stick nav bar with scroll and opacity 0.4

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,5 +1,39 @@
+$(window).load(function() {
+  $(".wrapper").delay(2000).animate({
+    opacity:0
+  }, 1000);
+});
+
+setTimeout(function(){
+  $(".wrapper").remove();
+}, 3400)
+
 $(document).ready(function() { 
   $('.word').each(function(i) {
-    $(this).delay(i * 500).fadeIn(1000);
+    $(this).delay(i * 450).fadeIn(700);
   });
 });
+
+$(document).ready(function(){
+  $('.kerrigan').delay(3250).fadeIn(800);
+})
+
+$(document).ready(function() {  
+  var stickyNavTop = $('.nav').offset().top;  
+    
+  var stickyNav = function(){  
+    var scrollTop = $(window).scrollTop();  
+           
+    if (scrollTop > stickyNavTop) {   
+      $('.nav').addClass('sticky');  
+    } else {  
+      $('.nav').removeClass('sticky');   
+    }  
+  };  
+    
+  stickyNav();  
+    
+  $(window).scroll(function() {  
+      stickyNav();  
+  });  
+});  
