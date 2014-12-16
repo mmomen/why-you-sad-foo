085fbc7bc7840bd727efe4086a9e28099774fc00
Mazdak Momen <mazdak.momen@gmail.com>
2014-12-15 17:07:32 -0800

    Adding div for the button and changing the buttonExec jquery to append to that div.

--- public/js/buttonExec.js
+++ public/js/buttonExec.js
@@ -9,7 +9,7 @@ var redirectURL = "/done";
 
 var form = $('<form action="' + redirectURL + '" method="post">' + '<div class="submitting">' + '<input type="submit" id="theClick" value="I ain\'t sad no mo" class="button radius">' + '</div>');
 
-$('.row').append(form);
+$('.button').append(form);
 
 $("#theClick").on("click", function() {
   var hiddenAttr = $('<input type="hidden" name="elapsed_time" value="' + value() + '"></input>');
